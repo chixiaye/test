2333a80d6b3e97d0269be0a8c602192beaf33ab2
chixiaye <xychichina@gmail.com>
2021-08-04 18:38:56 +0800

    test for reverse extract var

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class test {
+    public static void main(String[] args) throws Exception {
+        String buggyPath ="hello";
+        System.out.println(buggyPath);
+    }
+}

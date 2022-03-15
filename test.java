5c2444e04ed58a84610dae154ec78a6f740b8d63
chixiaye <54989108+chixiaye@users.noreply.github.com>
2022-03-15 11:02:27 +0800

    Update test.java

--- test.java
+++ test.java
@@ -6,6 +6,16 @@ import java.util.Set;
 public class test {
     public static void main(String[] args) throws Exception {
         String buggyPath ="hello";
+        if(f()>0) {
+            if(f()<0){
+                
+            }else if(f()==0){
+                
+            }
+        }
         System.out.println(buggyPath);
+    }
+     public static int f(){
+        return 1+1;
     }
 }
